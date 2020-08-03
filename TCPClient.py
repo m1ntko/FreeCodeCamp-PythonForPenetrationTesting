637d18c427592512aa4d8d5331762990a9806115
m1ntko <chetaniguzman@gmail.com>
2020-08-03 19:00:32 -0400

    Its works

--- TCPClient.py
+++ TCPClient.py
@@ -5,15 +5,15 @@ import socket
 # Creating the socket object. AF_INET -> ipv4, socket.SOCK_STREAM -> TCP
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# IP of the server
+# IP of the server, but this below doesnt work exactly, so put the server IP
 host = socket.gethostname()
 port = 444
 
-clientsocket.connect((host, port))
+clientsocket.connect(('192.168.1.56', port))
 
 # Max of data we are goint to accept
 message = clientsocket.recv(1024)
-
+ 
 clientsocket.close()
 
 print(message.decode('ascii'))
--- TCPServer.py
+++ TCPServer.py
@@ -5,12 +5,12 @@ import socket
 # Creating the socket object. AF_INET -> ipv4, socket.SOCK_STREAM -> TCP
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# host = '<IP>'
+# host = '<IP>' If you do not exactly use same method on the client side, you will get the error: socket.error errno 111 connection refused.
 host = socket.gethostname()
 port = 444
 
 # To bind host and port to server
-serversocket.bind((host, port))
+serversocket.bind(('192.168.1.56', port))
 
 # Set up TCP listener and how many connections/request 
 serversocket.listen(3)
@@ -27,4 +27,4 @@ while True:
     clientsocket.send(message.encode('ascii'))
 
     # Finish connection
-    clientsocket.close
+    clientsocket.close()
